008b2a4b124dcfc2a60aecc217eaa46ae0af9e98
VLinkus <LinkusVyte@gmail.com>
2023-12-05 16:06:34 +0200

    M6 Task1 Completed

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,44 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public Animal() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public boolean getHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return "This animal is mostly "+ color+". It has "+ numberOfPaws+(numberOfPaws>1?" paws": " paw")+" and "+ (hasFur? "a":"no")+" fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,11 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
-
+public class Bird  extends  Animal{
+    public Bird() {
+        super("blue", 2, false);
+    }
+    @Override
+    public String getDescription() {
+        return "This animal is mostly "+ getColor()+". It has "+ getNumberOfPaws()+(getNumberOfPaws()>1?" paws": " paw")+" and "+ (getHasFur()? "a":"no")+" fur. Moreover, it has 2 wings and can fly.";
+    }
 }
